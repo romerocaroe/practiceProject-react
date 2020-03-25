56a38de6e92c0680623e05cd54b692bdb5fe7595
Carolina Romero <carolina.romero@sondeos.com.ar>
2020-03-25 15:27:06 -0300

    Consigna finalizada, esperando feedback

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -6,20 +6,20 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-//import {IconButton} from '@material-ui/core'
-//import StarIcon from '@material-ui/icons/Star';
+import {IconButton} from '@material-ui/core'
+import StarIcon from '@material-ui/icons/Star';
 import { yellow, grey } from '@material-ui/core/colors';
 import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 400,
-    maxHeight: 600,
+    maxWidth: 300,
+    maxHeight: 550,
   },
   media: {
     maxWidth: 300,
     minWidth: 200,
-    maxHeight: 350,
+    maxHeight: 400,
     minHeight: 250,
   }}));
 
@@ -28,11 +28,11 @@ let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
 export default function BookCard(book) {
-  //const [starIcon, setStarIcon] = useState(false);
+  const [starIcon, setStarIcon] = useState(false);
   const classes = useStyles();
-  /*const onClick = () => {
+  const onClick = () => {
     setStarIcon(!starIcon)
-  };*/
+  };
 
   return (
     <div className="BookCard" style={{textAlign: 'center'}}>
@@ -54,13 +54,13 @@ export default function BookCard(book) {
                   </Typography>
               </CardContent>
           </CardActionArea>
-          <CardActions>
-              <Grid>
-                {/*<IconButton onClick={onClick()} style={{color: starIcon ? yellow[600] : grey}}>
-                  <StarIcon />
-                </IconButton>*/}
-              </Grid>
-          </CardActions>
+          <div style={{align: 'center'}}>
+            <CardActions>
+                  <IconButton onClick={onClick} style={{color: starIcon ? yellow[600] : grey[600]}}>
+                    <StarIcon fontSize="large" />
+                  </IconButton>
+            </CardActions>
+          </div>
         </Card>
     </div>
   );
--- src/components/ShowBook.js
+++ src/components/ShowBook.js
@@ -3,14 +3,12 @@ import BookCard from './BookCard';
 import Grid from '@material-ui/core/Grid';
 
 export default function ShowBook(props) {
-    console.log('showbook');
-    console.log(props.books);
     return(
         <div className="ShowBook">
             <Grid style={{padding:'24px', color: '#455a64'}} container spacing={10}>
             {(props.books.length > 0) ? (
                 (props.books.map((book, index) =>
-                    <Grid key={index} item xs={12} sm={4} md={4} lg={3}>
+                    <Grid key={index} item xs={12} sm={4} md={4} lg={4}>
                     <BookCard title={book.title} author={book.author} img={book.img} />
                     </Grid>
                 ))
