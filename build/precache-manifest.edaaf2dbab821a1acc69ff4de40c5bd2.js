2a18918005b28aad3c586e2517063ba35bab1c4a
Carolina Romero <carolina.romero@sondeos.com.ar>
2020-04-01 01:23:26 -0300

    Estrella corregida, responsive para todas las pantallas

---
+++ build/precache-manifest.edaaf2dbab821a1acc69ff4de40c5bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80272384072883e15694880dcc4c2f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e2ac2927054df03646",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "677b83c3e732f5ce5bca",
+    "url": "/static/js/2.2a900ded.chunk.js"
+  },
+  {
+    "revision": "7d9fa850aa2ff6b3a58494c222b6b450",
+    "url": "/static/js/2.2a900ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e2ac2927054df03646",
+    "url": "/static/js/main.bf45259a.chunk.js"
+  },
+  {
+    "revision": "6f35bd3f2f9c9e53e481",
+    "url": "/static/js/runtime-main.de706ec4.js"
+  }
+]);
