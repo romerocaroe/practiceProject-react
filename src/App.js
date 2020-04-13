d849d15ac0793750df05cf3fdfc0d2e169379ec4
Carolina Romero <carolina.romero@sondeos.com.ar>
2020-04-12 22:34:51 -0300

    Componente Navbar con busqueda de favs y libros (solo vista)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import ShowBook from './components/ShowBook';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import teal from '@material-ui/core/colors/teal';
-import grey from '@material-ui/core/colors/grey';
+import Navbar from './components/Navbar';
 
 function App() {
   const [books, setBooks] = useState([]);
-
+  
   //fetch with the backend
   useEffect(() => {
-    fetch('http://'+process.env.REACT_APP_BACK_URL+'/api/books')
+    fetch(process.env.REACT_APP_BACK_URL+'/api/books')
     .then((response) => {
       return response.json();
     })
@@ -22,32 +17,9 @@ function App() {
     .catch(err => console.log(err))
   },[]);
 
-  //header
-    const useStyles = makeStyles(theme => ({
-      root: {
-        flexGrow: 6,
-        minWidth: "100%",
-      },
-      palette: {
-        primary: teal[400],
-        secondary: grey[800]
-      }
-    }));
-    const classes = useStyles();
-
   return(
     <div className="App">
-      <header>
-        <div className={classes.root}>
-            <AppBar position="static" style={{background: '#ffc107'}}>
-                <Toolbar variant="dense">
-                <Typography variant="h6" style={{color: '#616161'}}>
-                  La biblioteca de Sondeos
-                </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-      </header>
+        <Navbar />
       <div className="ShowBook">
         <ShowBook books={books} />
       </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import {IconButton} from '@material-ui/core'
+import StarIcon from '@material-ui/icons/Star';
+import teal from '@material-ui/core/colors/teal';
+import grey from '@material-ui/core/colors/grey';
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default function Navbar() {
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      minWidth: "100%",
+    },
+    palette: {
+      primary: teal[400],
+      secondary: grey[800]
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+  const classes = useStyles();
+
+  return(
+    <header>
+      <div className={classes.root}>
+        <AppBar position="static" style={{background: '#4a148c'}}>
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.title}>
+              La biblioteca de Sondeos
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+            {/*fav icon*/}
+            <IconButton color="inherit">
+              <StarIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </header>
+  )
+}
